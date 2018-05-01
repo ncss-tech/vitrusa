b7d95d334856c32667ec7b01a3dd84a1f734e8a8
John Hammerly <hammerly@users.noreply.github.com>
2018-05-01 10:54:22 -0400

    removed files

--- sandbox/john/moisture_query/site_soil_moist_plot.R
+++
@@ -1,3 +0,0 @@
-library(lubridate)
-s_by_month<- aggregate(cbind(t_sm_d)~month(obs_date),data=s,FUN=mean)
-ggplot(s_by_month, aes(x= `month(obs_date)`,y= t_sm_d))+geom_line(cex=1)+scale_y_reverse()+scale_x_continuous(breaks = 1:12, labels = month.abb, name="Month")+ylim(200, 0)
